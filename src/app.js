9faf3c1c731eb75c2b7c4fddefac4d487280982a
aditya <adityatandulkar20@gmail.com>
2023-04-29 13:08:59 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 // Importing products from products.json file
-const products = JSON.parse(
+let products = JSON.parse(
     fs.readFileSync(`${__dirname}/data/product.json`)
 );
 
@@ -14,6 +14,48 @@ app.use(express.json());
 
 // Write PATCH endpoint to buy a product for the client here
 // Endpoint /api/v1/products/:id
+
+
+app.patch("/api/v1/products/:id", (req, res)=>{
+    const {id} = req.params;
+
+    let item = products.find((item)=>(item.id === Number(id)))
+
+    if(item){
+        const quantity = item.quantity;
+        item = {...item, ["quantity"] : quantity-1};
+
+        if(quantity === 0){
+            res.status(404).json({
+                "status": "success",
+                "message": `Product ${id}, Out of stock!`,
+            })
+        }
+        else{
+            products.forEach((elem, idx)=>{
+                if(elem.id === item.id){
+                    products[idx] = {...item};
+
+                    fs.writeFileSync(`${__dirname}/data/product.json`, JSON.stringify(products));
+
+                    res.status(200).json({
+                        "status": "success",
+                        "message": "Thank you for purchasing Product "+id,
+                        "product": item
+                    });
+
+                    return;
+                }
+            })
+        }
+    }
+    else{
+        res.status(404).json({
+            "status": "failed",
+            "message": "Product not found!"
+        })
+    }
+})
 
 
 
